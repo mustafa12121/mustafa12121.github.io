c5f6606437faf57101c653c1777d9d4cebf0e8a4
MustafaEsmail <mustafaasmael1234@gmail.com>
2023-02-07 20:04:46 +0200

    adding the main files

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const pageRuning = {
+  isRuning: true,
+  inDevContenet: "Sorry we are working on it",
+};
+
+if (!pageRuning.isRuning) {
+  document.body.innerHTML = "";
+  let div = document.createElement("div");
+  div.className = "development--inwork";
+  let wotch = document.createElement("span");
+  wotch.append("⏳");
+  wotch.className = "wotch";
+  let content = pageRuning.inDevContenet;
+  div.append(content, wotch);
+  document.body.append(div);
+}
