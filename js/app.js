c2f3288c2ef58adb619cf0e31b5f0b47e7575948
MustafaEsmail <mustafaasmael1234@gmail.com>
2023-02-08 11:41:14 +0200

    progrecessing in the portofolio

--- js/app.js
+++ js/app.js
@@ -3,6 +3,44 @@ const pageRuning = {
   inDevContenet: "Sorry we are working on it",
 };
 
+let work = document.getElementById("work");
+//most be an array
+let works = ["Front-End developer", "Networker"];
+typeEffect(works, work, { add: 150, remove: 150 });
+
+function typeEffect(
+  text,
+  contanier,
+  time = { add: 150, remove: 100 },
+  conter = 0
+) {
+  contanier.innerHTML = "";
+  let chars = Array.from(text[conter]);
+  let i = 0;
+  let adder = setInterval(() => {
+    contanier.append(chars[i]);
+    i++;
+    if (i === chars.length) {
+      clearInterval(adder);
+      setTimeout(() => {
+        let deleter = setInterval(() => {
+          chars.length = i;
+          contanier.innerHTML = chars.join("");
+          if (i == 0) {
+            clearInterval(deleter);
+            setTimeout(() => {
+              if (conter === text.length - 1)
+                typeEffect(text, contanier, time, 0);
+              else typeEffect(text, contanier, time, ++conter);
+            }, 1000);
+          }
+          i--;
+        }, time.remove);
+      }, 1000);
+    }
+  }, time.add);
+}
+
 if (!pageRuning.isRuning) {
   document.body.innerHTML = "";
   let div = document.createElement("div");
